d1594511b491be12e1e725ec3c531e4bc23678f2
AzamatAliqulov <40868285+alikulov-dev@users.noreply.github.com>
2021-11-11 11:06:28 +0500

    Update client.js

--- db/models/client.js
+++ db/models/client.js
@@ -52,7 +52,7 @@ const clientSchema = new mongoose.Schema({
     lowercase: true,
     unique: true,
     required: 'Email address is required',
-    validate: [validateEmail, 'Please fill a valid email address'],
+    //validate: [validateEmail, 'Please fill a valid email address'],
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   img_id: {
